ae6196435df2dc60b7ab94da91fa38652505acc3
Elshat <z90602190@hotmail.com>
2022-06-18 18:33:33 +0300

    DAY 14 - DRYRUN- TAGS- HTML REPORT-2

--- src/test/java/com/cydeo/step_definitions/login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/login_StepDefinitions.java
@@ -19,4 +19,16 @@ public class login_StepDefinitions {
         System.out.println("user should see the dashboard");
     }
 
+
+    @When("user enters student username")
+    public void user_enters_student_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters student password")
+    public void user_enters_student_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
