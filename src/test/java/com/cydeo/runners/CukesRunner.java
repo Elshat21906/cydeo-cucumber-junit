8492bbda97b1d545fb0e973514d2ac8e834f0fd7
Elshat <z90602190@hotmail.com>
2022-06-18 20:28:22 +0300

    DAY 14 - DRYRUN- TAGS- HTML REPORT-2

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,10 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = true,
-        tags = "@Regression"
+        tags = "@Regression and not @student"
 )
 
 public class CukesRunner {
