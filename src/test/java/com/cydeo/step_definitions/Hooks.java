c633abb96146bc417bede7cd925be9606bf3d342
Elshat <z90602190@hotmail.com>
2022-06-19 12:47:24 +0300

    DAY15 - BACKGROUND-HOOKS

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -13,16 +13,16 @@ import io.cucumber.java.BeforeStep;
 public class Hooks {
 
     // import from io.cucumber.java not
-    @Before
+    @Before(order = 1)
     public void setupScenario(){
         System.out.println("====Setting up browser using cucumber @Before");
     }
-    @Before("@login")
+    @Before(value = "@login",order = 2)
     public void setupScenarioForLogin(){
         System.out.println("====this will only apply to scenario with @login tag");
     }
 
-    @Before("@db")
+    @Before(value = "@db",order = 0)
     public void setupForDataBaseScenario(){
         System.out.println("====this will only apply to scenario with @db tag");
     }
