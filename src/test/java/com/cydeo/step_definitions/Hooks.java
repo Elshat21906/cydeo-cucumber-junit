2423db82248587219f8a4386cadc8b8ccf28e493
Elshat <z90602190@hotmail.com>
2022-06-19 12:06:01 +0300

    DAY15 - BACKGROUND-HOOKS

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,17 @@
+package com.cydeo.step_definitions;
+
+/*
+In the class we will be able to pass pre- & post- conditions to
+ each scenario and each step
+ */
+
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    // import from io.cucumber.java not
+    @Before
+    public void setupScenario(){
+        System.out.println("====Setting up browser using cucumber @Before");
+    }
+}
