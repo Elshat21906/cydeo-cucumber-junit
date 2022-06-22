a456ff428767690d1a4db2219d60eee2fb7cf793
Elshat <z90602190@hotmail.com>
2022-06-22 17:05:00 +0300

    DAY16 -PARAMETERIZATION-DATA TABLES-SCENARIO OUTLINES

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,19 +1,29 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.bytebuddy.asm.Advice;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
 
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
     @When("user types apple and clicks enter")
     public void user_types_apple_and_clicks_enter() {
-
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
+        String expectedTitle = "apple - Google 搜索";
+        String actualTitle = Driver.getDriver().getTitle();
 
+        // Junit assertion accepts first arg as expected,second arg as actual
+        Assert.assertEquals(expectedTitle,actualTitle);
+        //Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
 
 
