68c199eaeffdd2acf667e7c3d0256ea593fdf3e6
Elshat <z90602190@hotmail.com>
2022-06-23 17:54:59 +0300

    DAY16 -PARAMETERIZATION-DATA TABLES-SCENARIO OUTLINES

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -13,12 +13,17 @@ public class GoogleStepDefinitions {
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
     @When("user types {string} and clicks enter")
-    public void user_types_and_clicks_enter(String string) {
-
+    public void user_types_and_clicks_enter(String searchKeyword) {
+        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
     }
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
 
+        String expectedTitle = string + "- Google 搜索";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        // Junit assertion accepts first arg as expected,second arg as actual
+        Assert.assertEquals("Title is not as expected",expectedTitle,actualTitle);
     }
 
 
