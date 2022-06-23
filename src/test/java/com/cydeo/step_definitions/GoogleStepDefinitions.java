dd4370d770b178d440dff3c8d956fb01e334bca4
Elshat <z90602190@hotmail.com>
2022-06-23 18:23:59 +0300

    DAY16 -PARAMETERIZATION-DATA TABLES-SCENARIO OUTLINES

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -12,6 +12,11 @@ public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+    @When("user types {word} and clicks enter")
+    public void user_types_and_clicks_enter_2(String searchKeyword) {
+        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
+    }
+
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String searchKeyword) {
         googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
@@ -33,9 +38,9 @@ public class GoogleStepDefinitions {
     public void user_types_apple_and_clicks_enter() {
         googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
-    @Then("user sees apple in the google title")
-    public void user_sees_apple_in_the_google_title() {
-        String expectedTitle = "apple - Google 搜索";
+    @Then("user sees {word} in the google title")
+    public void user_sees_apple_in_the_google_title(String word) {
+        String expectedTitle = word + "- Google 搜索";
         String actualTitle = Driver.getDriver().getTitle();
 
         // Junit assertion accepts first arg as expected,second arg as actual
