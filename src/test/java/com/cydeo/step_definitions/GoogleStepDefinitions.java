2320960027b193958300dcb2505ae1de92f82474
Elshat <z90602190@hotmail.com>
2022-06-23 18:36:27 +0300

    DAY16 -PARAMETERIZATION-DATA TABLES-SCENARIO OUTLINES

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -12,15 +12,16 @@ public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
-    @When("user types {word} and clicks enter")
-    public void user_types_and_clicks_enter_2(String searchKeyword) {
-        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
+    @When("user types apple and clicks enter")
+    public void user_types_and_clicks_enter_2() {
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
 
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String searchKeyword) {
         googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
     }
+
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
 
@@ -33,14 +34,10 @@ public class GoogleStepDefinitions {
 
 
 
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
 
-    @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
-        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
-    }
-    @Then("user sees {word} in the google title")
-    public void user_sees_apple_in_the_google_title(String word) {
-        String expectedTitle = word + "- Google 搜索";
+        String expectedTitle = "apple" + "- Google 搜索";
         String actualTitle = Driver.getDriver().getTitle();
 
         // Junit assertion accepts first arg as expected,second arg as actual
@@ -48,11 +45,11 @@ public class GoogleStepDefinitions {
         //Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
 
-
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
         Driver.getDriver().get("https://www.google.com");
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
         String expectedTitle = "Google";
