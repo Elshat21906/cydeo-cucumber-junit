652eec7c27c7b4e6fd6295beb65a0356393858b6
Elshat <z90602190@hotmail.com>
2022-06-25 17:50:22 +0300

    DAY17-PARAMETERIZATION - DATATABLES REVIEW

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,11 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
-        features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions",
-        dryRun = false,
-        tags = "@ScenarioOutline"
+        plugin = "html:target/cucumber-report.html",//determines what type of report we want to generate with our project, and also where we want to store.
+        features = "src/test/resources/features",// we provide the path of the 'features' directory and let our project know where to find all the feature files.
+        glue = "com/cydeo/step_definitions",//glue gives path to the package of the step_definitions
+        dryRun = false,// dryRun determines if we want to execute the step_definitions or not
+                       // if dryRun is true: dryRun is turned on, and step definitions are turned off (will not run)
+                       // if dryRUn is false: dryRun is turned off, and step definitions are turned on (will run)
+        tags = "@ScenarioOutline" // What are tags and why we use them?
+                                  // Tag allows us to create different scenario suites or groups to run
+                                  // we can include, or exclude different feature files or scenarios using tags.
+
 )
 
 public class CukesRunner {
