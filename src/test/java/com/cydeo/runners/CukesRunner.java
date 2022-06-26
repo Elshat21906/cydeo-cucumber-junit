abc95368131d4f0e32a2a6dd978b26ef34e400c3
Elshat <z90602190@hotmail.com>
2022-06-26 17:50:58 +0300

    DAY18-Parallel Testing

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,16 +6,25 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",//determines what type of report we want to generate with our project, and also where we want to store.
-        features = "src/test/resources/features",// we provide the path of the 'features' directory and let our project know where to find all the feature files.
-        glue = "com/cydeo/step_definitions",//glue gives path to the package of the step_definitions
-        dryRun = false,// dryRun determines if we want to execute the step_definitions or not
-                       // if dryRun is true: dryRun is turned on, and step definitions are turned off (will not run)
-                       // if dryRUn is false: dryRun is turned off, and step definitions are turned on (will run)
-                       // this is mostly used for when implementing new steps and step definitions, and we do not want to run the code and open browser etc.,just to get the snippets.
-        tags = "@maleScientist" // What are tags and why we use them?
-                                  // Tag allows us to create different scenario suites or groups to run
-                                  // we can include, or exclude different feature files or scenarios using tags.
+        //determines what type of report we want to generate with our project, and also where we want to store.
+        plugin = "html:target/cucumber-report.html",
+
+        // we provide the path of the 'features' directory and let our project know where to find all the feature files.
+        features = "src/test/resources/features",
+
+        //glue gives path to the package of the step_definitions
+        glue = "com/cydeo/step_definitions",
+
+        // dryRun determines if we want to execute the step_definitions or not
+        // if dryRun is true: dryRun is turned on, and step definitions are turned off (will not run)
+        // if dryRUn is false: dryRun is turned off, and step definitions are turned on (will run)
+        // this is mostly used for when implementing new steps and step definitions, and we do not want to run the code and open browser etc.,just to get the snippets.
+        dryRun = false,
+
+        // What are tags and why we use them?
+        // Tag allows us to create different scenario suites or groups to run
+        // we can include, or exclude different feature files or scenarios using tags.
+        tags = "@maleScientist"
 )
 public class CukesRunner {
 
