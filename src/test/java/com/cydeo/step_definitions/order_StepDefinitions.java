082d16493d4c53b4f28f4cac1f82c78af0889b05
Elshat <z90602190@hotmail.com>
2022-06-26 15:08:07 +0300

    DAY18-Parallel Testing

--- src/test/java/com/cydeo/step_definitions/order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/order_StepDefinitions.java
@@ -5,6 +5,7 @@ import com.cydeo.pages.OrderPage;
 import com.cydeo.pages.WebTableLoginPage;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -35,10 +36,20 @@ public class order_StepDefinitions {
         Select select = new Select(orderPage.productDropdown);
         select.selectByVisibleText(string);
     }
-    @When("user enters quantity {int}")
-    public void user_enters_quantity(Integer int1) {
+   // @When("user enters quantity {int}")
+   // public void user_enters_quantity(Integer int1) {
+   // }
 
+    @And("user enters quantity {int}")
+    public void userEntersQuantity(int arg0) {
+        //accepting int argument and sending it using sendKeys() method
+        //since sendKeys() method only accepts String, we need to either concat with ""
+        //or send String.valueOf(int);
+
+        orderPage.inputQuantity.sendKeys(arg0 + "");
+        orderPage.inputQuantity.sendKeys(String.valueOf(arg0));
     }
+
     @When("user enters customer name {string}")
     public void user_enters_customer_name(String string) {
 
@@ -79,5 +90,6 @@ public class order_StepDefinitions {
     public void user_should_see_in_first_row_of_the_web_table(String string) {
 
     }
+
 
 }
