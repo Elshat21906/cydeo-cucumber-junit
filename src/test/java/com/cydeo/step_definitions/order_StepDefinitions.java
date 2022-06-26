811a52c93a22ea641345ab70a0be9d8c2c1dfe81
Elshat <z90602190@hotmail.com>
2022-06-26 16:44:05 +0300

    DAY18-Parallel Testing

--- src/test/java/com/cydeo/step_definitions/order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/order_StepDefinitions.java
@@ -66,22 +66,27 @@ public class order_StepDefinitions {
     public void user_enters_customer_name(String string) {
         orderPage.inputName.sendKeys(string);
     }
+
     @When("user enters street {string}")
     public void user_enters_street(String string) {
         orderPage.inputStreet.sendKeys(string);
     }
+
     @When("user enters city {string}")
     public void user_enters_city(String string) {
         orderPage.inputCity.sendKeys(string);
     }
+
     @When("user enters state {string}")
     public void user_enters_state(String string) {
         orderPage.inputState.sendKeys(string);
     }
+
     @When("user enters zipcode {string}")
     public void user_enters_zipcode(String string) {
         orderPage.inputZip.sendKeys(string);
     }
+
     @When("user selects credit card type {string}")
     public void user_selects_credit_card_type(String expectedCardTypes) {
 
@@ -92,16 +97,20 @@ public class order_StepDefinitions {
     public void user_enters_credit_card_number(String string) {
         orderPage.cardNoInput.sendKeys(string);
     }
+
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
         orderPage.cardExpInput.sendKeys(string);
     }
+
     @When("user enters process order button")
     public void user_enters_process_order_button() {
         orderPage.processOrderBtn.click();
     }
+
     @Then("user should see {string} in first row of the web table")
     public void user_should_see_in_first_row_of_the_web_table(String string) {
+
 
     }
 
