97b88475b3a7a1140e333ea1327f9a702d4ac0c9
Elshat <z90602190@hotmail.com>
2022-06-26 19:36:44 +0300

    DAY18-Parallel Testing

--- src/test/java/com/cydeo/runners/FailedTestRunner.java
+++ src/test/java/com/cydeo/runners/FailedTestRunner.java
@@ -6,7 +6,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions
+@CucumberOptions(
+        glue = "com/cydeo/step_definitions",
+        features = "@target/rerun.txt"
+)
 public class FailedTestRunner {
 
 }
