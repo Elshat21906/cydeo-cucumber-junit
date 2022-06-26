d14b1e0e2f9a7e48f5e661254490e93a647fb0b0
Elshat <z90602190@hotmail.com>
2022-06-26 08:43:36 +0300

    DAY17-PARAMETERIZATION - DATATABLES REVIEW

--- src/test/java/com/cydeo/pages/WebTableLoginPage.java
+++ src/test/java/com/cydeo/pages/WebTableLoginPage.java
@@ -34,4 +34,16 @@ public class WebTableLoginPage {
         this.loginButton.click();
     }
 
+    /**
+     * This method will accept two arguments and login.
+     * @param username
+     * @param password
+     */
+    public void login(String username,String password){
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        loginButton.click();
+
+    }
+
 }
